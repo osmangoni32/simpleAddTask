c5ca327545676c0f981ea745b7521f7c5dde4c30
osman goni <osman.ice.32@gmail.com>
2024-01-24 13:35:49 +0600

    functionality added

--- src/components/TaskManagement.jsx
+++ src/components/TaskManagement.jsx
@@ -4,7 +4,27 @@ import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 export default function TaskManagement() {
   const [tasks, setTasks] = useState(initialTasks);
-  
+  const nextId = (data) => {
+    const maxid = data.reduce(
+      (prev, current) =>
+        (prev && prev.id) > current.id ? prev.id : current.id,
+      null
+    );
+    return maxid ? maxid + 1 : 1;
+  };
+
+  const handleAddTask = (text) => {
+    setTasks([
+      ...tasks,
+      {
+        id: nextId(tasks),
+        text: text,
+        done: false,
+      },
+    ]);
+   
+  };
+ 
 
   return (
     <>
